afd4051b3bd3bc6748a21a08a34b7ec9ff3c7f68
Victoria Thorsen <victoriathorsenn@gmail.com>
2021-02-28 11:48:26 -0500

    commit tree 57c6212f486c7cd1038983b1f5ad1deeb50bcd03

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,10 +7,14 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
